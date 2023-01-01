a24abc10da8860b6943bd52578539dd34c8ecfb3
Elyas Asmad <elyasasmadz@gmail.com>
2023-01-01 18:44:53 +0800

    ✅ Change request body type to JSON & DB connect

--- routes/auth.go
+++ routes/auth.go
@@ -1,39 +1,92 @@
 package routes
 
 import (
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-gonic/gin"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"strings"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+
+	"ServerLoginAuth/services"
 )
 
+type RegisterJSONBody struct {
+	FullName     string `json:"fullName"`
+	Username     string `json:"username"`
+	EmailAddress string `json:"emailAddress"`
+	Password     string `json:"password"`
+	PhoneNumber  string `json:"phoneNumber"`
+}
+
+type LoginJSONBody struct {
+	EmailAddress string `json:"emailAddress"`
+	Password     string `json:"password"`
+}
+
 func Register(ctx *gin.Context) {
+
+	reqBytes, reqErr := io.ReadAll(ctx.Request.Body)
+
+	var requestBody RegisterJSONBody
+
+	if reqErr != nil {
+		panic(reqErr)
+	}
+
+	if err := json.Unmarshal(reqBytes, &requestBody); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(requestBody)
+
+	services.RegisterUser(requestBody.FullName, requestBody.Username, requestBody.EmailAddress, requestBody.Password, requestBody.PhoneNumber)
+
+	ctx.JSON(http.StatusCreated, gin.H{"message": "Successfully creted user!"})
 
 }
 func Login(ctx *gin.Context) {
 	session := sessions.Default(ctx)
-	username := ctx.PostForm("username")
-	password := ctx.PostForm("password")
+
+	reqBytes, reqErr := io.ReadAll(ctx.Request.Body)
+
+	var requestBody LoginJSONBody
+
+	if reqErr != nil {
+		panic(reqErr)
+	}
+
+	if err := json.Unmarshal(reqBytes, &requestBody); err != nil {
+		panic(err)
+	}
 
 	// Validate form input
-	if strings.Trim(username, " ") == "" || strings.Trim(password, " ") == "" {
+	if strings.Trim(requestBody.EmailAddress, " ") == "" || strings.Trim(requestBody.Password, " ") == "" {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Parameters can't be empty"})
 		return
 	}
 
 	// Check for username and password match, usually from a database
-	if username != "1729963" || password != "aimanrahim" {
-		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed"})
+	// if username != "1729963" || password != "aimanrahim" {
+	// 	ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed"})
+	// 	return
+	// }
+	loginResult := services.TryLogin(requestBody.EmailAddress, requestBody.Password)
+
+	if loginResult.Error {
+		ctx.JSON(http.StatusUnauthorized, loginResult)
 		return
 	}
 
 	// Save the username in the session
-	session.Set("user", username) // In real world usage you'd set this to the users ID
+	session.Set("user", loginResult.Data.Uid) // In real world usage you'd set this to the users ID
 	if err := session.Save(); err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
 		return
 	}
-	ctx.JSON(http.StatusOK, gin.H{"message": "Successfully authenticated user"})
+	ctx.JSON(http.StatusOK, loginResult)
 }
 
 func Logout(ctx *gin.Context) {
